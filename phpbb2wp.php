a506adfdc057e9b94a75dc002377653aa7dd1726
JackNUMBER <antoine.cdrt@gmail.com>
2013-10-15 00:00:45 +0200

    First release
    Version 0.1 - No UI

---
+++ phpbb2wp.php
@@ -0,0 +1,261 @@
+<?php
+/*
+ * PHPBB2WP
+ * Migrate phpBB forum to WP blog
+ * Version 0.1 - No UI
+ * By Colin Braly (@_awk) & Antoine Cadoret (@JackNUMBER)
+ *
+ * HOW TO:
+ * 1. Install Wordpress on your phpBB server.
+ * 2. Put the file into the root folder.
+ * 3. Run it. 
+ *
+ * TODO:
+ * - Clean scripts comments & translate
+ * - Create UI
+ * - Use PDO interface
+ * - Check phpBB database
+ * - Check phpBB install
+ * - Check Wordpress database
+ * - Check Wordpress install
+ * - Add comments keep/kill option
+ * - Add old database keep/kill option
+ * - Add domain new/old option
+ * - Add BBcode manager (from bbcode table ?)
+ * - Add link manager //replace with new post id
+ * - Add smiley killer (from smileys table)
+ * - Add image manager system //simple field with new url? 
+ * - Add category manager
+ *
+ */
+?>
+
+<h1>Migrate phpBB2 to Wordpress</h1>
+<style>
+	.alert{background:#ff9;}
+	.warning{background:#f99;}
+	.success{background:#9f9;}
+</style>
+
+<?php
+/* Database connection */
+$mysql_host = 'localhost';
+$mysql_user = 'root';
+$mysql_pwd  = '';
+$mysql_db 	= 'phpbb2wp';
+
+$db_connect = mysql_connect($mysql_host, $mysql_user, $mysql_pwd)
+    or die('<p class="msg warning">Database connection failed: '.mysql_error().'</p>');
+mysql_query("SET NAMES 'utf8'", $db_connect);
+
+echo '<p class="success">Database connection successful</p>';
+
+mysql_select_db($mysql_db);
+
+/* ==================== Global Functions ==================== */
+
+/* BBcode URL tag standardization */
+function cleanURL($str) {
+	while (preg_match('#\[url\]http://(.*?)\[/url\]#', $str, $matches)) {
+		$tag = str_replace('[url]','[url=http://'.$matches[1].']', $matches[0]);
+		$str = str_replace($matches[0], $tag, $str);
+	}
+	while (preg_match('#\[url\]www.(.*?)\[/url\]#', $str, $matches)) {
+		$tag = str_replace('[url]','[url=http://www.'.$matches[1].']', $matches[0]);
+		$str = str_replace($matches[0], $tag, $str);
+	}
+	return $str;
+}
+
+/* List BBcode */
+function bbcode2html($str, $uid = '') {
+  $bbcode = array(
+                "[list$uid]", "[*$uid]", "[/list$uid]", 
+                "[list=1$uid]", "[/list:o$uid]", 
+                "[img$uid]", "[/img$uid]", 
+                "[b$uid]", "[/b$uid]", 
+                "[u$uid]", "[/u$uid]", 
+                "[i$uid]", "[/i$uid]",
+                '[color=', "[/color$uid]",
+                "[size=\"", "[/size$uid]",
+                "[size=", "[/size$uid]",
+                '[url=', "[/url]",
+                "[mail=\"", "[/mail]",
+                "[code]", "[/code]",
+                "[code:1$uid]", "[/code:1$uid]",
+                "[quote]", "[/quote]",
+	);
+  $htmlcode = array(
+                "<ul>", "<li>", "</ul>", 
+                "<ul>", "</ul>", 
+                "<img src=\"", "\">", 
+                "<strong>", "</strong>", 
+                "<u>", "</u>", 
+                "<em>", "</em>",
+                "<span style=\"color:", "</span>",
+                "<span style=\"font-size:", "</span>",
+                "<span style=\"font-size:", "</span>",
+                '<a href="', "</a>",
+                "<a href=\"mailto:", "</a>",
+                "<code>", "</code>",
+                "<code>", "</code>",
+                "<table width=100% bgcolor=lightgray><tr><td bgcolor=white>", "</td></tr></table>"
+	);
+  $newstr = str_replace('<', '&lt;', $str);
+	$newstr = str_replace('>', '&gt;', $newstr);
+  $newstr = str_replace($bbcode, $htmlcode, $newstr);
+  $newstr = nl2br($newstr);
+	return $newstr;
+}
+
+/* Convert BBcode to HTML */
+function bbcode2html2($str, $uid) {
+  $bbcode = array(
+                '"'.$uid.']',
+                '"]',
+                $uid.']',
+								']'
+	);
+  $htmlcode = array(
+                '">',
+                '">',
+                '">',
+                '">'
+	);
+  $newstr = str_replace($bbcode, $htmlcode, $str);
+	return $newstr;
+}
+
+/* List smilies */
+function killSmileys($str) {
+	$smileys = array (
+								/* Don't kill them 
+								":D",
+								":)",
+								":P",
+								":-|",
+								":(",*/
+								":shock:",
+								":?",
+								"8-)",
+								":lol:",
+								":oops:",
+								":cry:",
+								":evil:",
+								":twisted:",
+								":roll:",
+								":wink:",
+								":!:",
+								":?:",
+								":idea:",
+								":arrow:");
+	$newstr = str_replace($smileys, '', $str);
+	return $newstr;
+}
+function cleanCrochet($str) {
+	while (preg_match('#\[(.*?)">#', $str, $matches)) {
+		$tag = str_replace('">',']', $matches[0]);
+		$str = str_replace($matches[0], $tag, $str);
+	}
+	return $str;
+}
+
+/* ================================================== */
+
+/* ==================== Process ==================== */
+
+/* Posts reading */
+$sql = ' SELECT 
+					 phpbb_posts.post_id, 
+					 phpbb_posts.topic_id, 
+					 phpbb_posts.post_time, 
+					 phpbb_posts.post_edit_time, 
+					 phpbb_posts.forum_id,
+					 phpbb_posts_text.bbcode_uid,
+					 phpbb_posts_text.post_subject,
+					 phpbb_posts_text.post_text
+				 FROM 	 
+					 phpbb_posts, 
+					 phpbb_posts_text
+				 WHERE 
+				 	 phpbb_posts.post_id = phpbb_posts_text.post_id
+				 	 AND phpbb_posts.poster_id = 2
+				 ORDER BY phpbb_posts.post_time ASC
+				 LIMIT 10
+				';
+
+$result = mysql_query($sql);
+
+if (!$result) {
+	$message  = '<p class="msg warning">Invalid Request: '.mysql_error()."</p>";
+	die($message);
+}
+
+echo '<p>Posts reading...</p>';
+
+define('WP_USE_THEMES', false);
+if(file_exists('./wp-blog-header.php')){
+	require('./wp-blog-header.php');
+}else{
+	echo '<p class="msg warning">Wordpress isn\'t installed</p>';
+	break;
+}
+
+
+$article_wp = array();
+$topic_done = array();
+$cpt_article = 0;
+$converted = 0;
+
+while ($post_phpbb = mysql_fetch_assoc($result)) {
+	if (in_array($post_phpbb['topic_id'], $topic_done)) {
+		continue;
+	}
+	$topic_done[] = $post_phpbb['topic_id'];
+
+	/* Posts conversion */
+	
+	// Dates - Is GMT convertion needed?
+	$article_wp[$cpt_article]['post_date'] = date("Y-m-d H:i:s", $post_phpbb['post_time']); // wp_post.post_date
+	$article_wp[$cpt_article]['post_date_gmt'] = date("Y-m-d H:i:s", $post_phpbb['post_time_gmt']-(60*60)); // wp_post.post_date_gmt
+	$article_wp[$cpt_article]['wp_post.post_modified'] = date("Y-m-d H:i:s", $post_phpbb['post_edit_time']); // wp_post.post_modified
+	$article_wp[$cpt_article]['wp_post.post_modified_gmt'] = date("Y-m-d H:i:s", $post_phpbb['post_edit_time']-(60*60)); // wp_post.post_modified_gmt
+
+	// Title
+	$article_wp[$cpt_article]['post_title'] = $post_phpbb['post_subject']; // wp_posts.post_title
+
+	// Content
+	$post_phpbb['post_text'] = cleanURL($post_phpbb['post_text']);
+	$article_wp[$cpt_article]['post_content'] = bbcode2html($post_phpbb['post_text'], ':'.$post_phpbb['bbcode_uid']); // post_content
+	$article_wp[$cpt_article]['post_content'] = bbcode2html2($article_wp[$cpt_article]['post_content'], ':'.$post_phpbb['bbcode_uid']); // wp_posts.post_content
+	$article_wp[$cpt_article]['post_content'] = cleanCrochet($article_wp[$cpt_article]['post_content']); // wp_posts.post_content
+	$article_wp[$cpt_article]['post_content'] = killSmileys($article_wp[$cpt_article]['post_content']); // wp_posts.post_content
+	
+	// Category
+	$article_wp[$cpt_article]['wp_term_relationships.object_id'] = $post_phpbb['forum_id']; // wp_term_relationships.object_id
+
+	// Specifics Wordpress Meta
+	$article_wp[$cpt_article]['comment_status'] = 'open';
+	$article_wp[$cpt_article]['ping_status'] = 'open';
+	$article_wp[$cpt_article]['post_author'] = 2; // Antoine
+	$article_wp[$cpt_article]['post_status'] = 'publish';
+	$article_wp[$cpt_article]['post_type'] = 'post';
+
+	/* Create Wordpress posts */
+	if (wp_insert_post($article_wp[$cpt_article])) {
+		$converted++;
+	}else{
+		echo '<p class="warning">Error on writing the WP database</p>';
+	}
+	$cpt_article++;
+}
+
+echo '<p class="alert">';
+echo $cpt_article.' posts found.<br>';
+echo $converted.' posts converted.';
+echo '</p>';
+
+/* ================================================== */
+
+mysql_close($db_connect);
+?>
